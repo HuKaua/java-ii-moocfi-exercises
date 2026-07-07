2fad407dbbc35e5aaf313bd748ef48e606c86eab
Hugo Kauã <Nombasa@Adeles-MacBook-Pro-2.local>
2026-07-07 11:50:51 -0300

    feito exc 8

--- part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
+++ part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -7,4 +7,26 @@ public class Program {
         // Test your program here!
     }
 
+    public static void printKeys(HashMap<String,String> hashmap){
+        for (String text : hashmap.keySet()) {
+            System.out.println(text);
+        }
+    }
+
+    public static void printKeysWhere(HashMap<String, String> hashmap, String text){
+        for (String txt : hashmap.keySet()) {
+            if (txt.contains(text)) {
+                System.out.println(txt);
+            }
+        }
+    }
+
+    public static void printValuesOfKeysWhere(HashMap<String,String> hashmap, String text){
+        for (String txt: hashmap.keySet()) {
+            if (txt.contains(text)) {
+                System.out.println(hashmap.get(txt));
+            }
+        }
+    }
+
 }
